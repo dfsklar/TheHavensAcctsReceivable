122a8585c9cb28018e04ab39aca6d5153be49ef9
David Sklar <dfsklar@gmail.com>
2016-03-11 13:32:33 -0500

    WIP

--- main.rb
+++ main.rb
@@ -10,5 +10,9 @@ puts "Number to process: " + gmail.mailbox('vrbo_pending_processing').count.to_s
 gmail.mailbox('vrbo_pending_processing').emails.each do |email|
   body_in_html = email.message.body.to_s
   doc = Nokogiri::parse (body_in_html)
+  nodeset_all_tables = doc.xpath('//table')
+  the_table = nodeset_all_tables[4]
+  the_rows = the_table.xpath('tr')
+  puts the_rows[1].to_s
   break
 end
