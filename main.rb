07472589cd7b6c487df65f8184dcc7a4af561d10
David Sklar <dfsklar@gmail.com>
2016-03-12 16:43:14 -0500

    Removal of a label is done via deletion

--- main.rb
+++ main.rb
@@ -20,6 +20,8 @@ exchrate = money.exchange_to(:CAD).fractional / 100.0
 
 gmail = Gmail.new(Credentials::USERNAME, Credentials::PWD)
 
+puts gmail.labels.all.to_s
+
 puts "Number to process: " + gmail.mailbox('vrbo_pending_processing').count.to_s
 
 mysql_queue = [ ]
@@ -37,17 +39,22 @@ def create_mysql_row (date, customer, invoice, gross, balance, exch)
   INSERT INTO BHreceiptsFromCustomers 
   (InvoiceID, CustName, DateCheckin, PaymentMethod, PaymentDate, PaymentAmount, ExchRate_CdnToOneUSD, Commentary) 
   VALUES 
-  ('#{invoice}', '#{customer}', '#{date}', 'VRBO', '#{date}', #{balance}, #{exch}, 
-  'Gross rent b4 fees #{gross} - Checkin date unknown'
+  ('#{invoice}', '#{customer}', '#{date}', 'VRBO', '#{date}', #{balance}/1.05, #{exch}, 
+  'Gross #{gross} // Net incl GST #{balance} // Checkin date unknown'
   );
 END
-  puts cmd
+  cmd
 end
+
+
+mysql = Mysql.connect(Credentials::MYSQL['host'], Credentials::MYSQL['username'], Credentials::MYSQL['password'], 'sklarchin')
 
 
 gmail.mailbox('vrbo_pending_processing').emails.each do |email|
   accounting_date = email.message.date.to_s[0..9]
   body_in_html = email.message.body.to_s
+  
+  
   doc = Nokogiri::parse (body_in_html)
   nodeset_all_tables = doc.xpath('//table')
   the_table = nodeset_all_tables[4]
@@ -69,7 +76,7 @@ gmail.mailbox('vrbo_pending_processing').emails.each do |email|
         customer = cellval(cells[0])
         invoice = cellval(cells[1])
         grossrent = dollaramt
-        puts "Gross rent = #{balance}"
+        puts "Gross rent = #{grossrent}"
       when String
         puts "Balance was adjusted by #{rowtype}"
         balance = balance + dollaramt
@@ -80,16 +87,19 @@ gmail.mailbox('vrbo_pending_processing').emails.each do |email|
       rowtype = cellval(cells[0])
       if rowtype == 'Total'
         balance = celldollar(cells[1])
+        sqlcmd = create_mysql_row accounting_date, customer, invoice, grossrent, balance, exchrate
+        puts sqlcmd
+        mysql.query sqlcmd
       end
-      puts rowtype
     end
   end
+    
+  email.move_to "vrbo_processed", "vrbo_pending_processing"
+  email.flag :Deleted
+  email.flag 'Deleted'
+  email.flag 'deleted'
+  email.remove_label "vrbo_pending_processing"
   
-  create_mysql_row accounting_date, customer, invoice, grossrent, balance, exchrate
   break
 end
 
-
-
-
-# mysql = Mysql.connect(Credentials::MYSQL['host'], Credentials::MYSQL['username'], Credentials::MYSQL['password'], 'sklarchin')
